bf381f8b6eeb2d77eb8ddaa66a217e64d3023f09
Jose Angel <joseangel.velasco@yahoo.es>
2023-06-25 11:53:19 +0200

    updates

--- pyomo_example_constraint_fraction.py
+++ pyomo_example_constraint_fraction.py
@@ -1,10 +1,8 @@
 import pyomo.environ as pyo
 from pyomo.opt import SolverFactory, SolverStatus, TerminationCondition
 
-
 # model
 model = pyo.ConcreteModel()
-
 
 model.param_fuel_purchase = {'diesel': 12000}
 model.set_fuels = ['diesel']
@@ -13,18 +11,21 @@ model.set_bios = ['fame', 'hvo']
 model.set_centers = ['Center_A', 'Center_B']
 model.set_suppliers = ['Supplier_1', 'Supplier_2']
 
+model.param_max_bio_volume = {'fame': 1200, 'hvo': 1200}
+model.param_min_bio_volume = {'fame': 100, 'hvo': 100}
+
 model.var_bio_purchase = pyo.Var(model.set_fuels,
                                  model.set_centers,
                                  model.set_suppliers,
                                  model.set_fuel_types,
                                  model.set_bios,
-                                 domain=pyo.PositiveIntegers)
+                                 domain=pyo.NonNegativeIntegers, initialize=0)
 
 model.param_fuel_energy_conversion = {'diesel': 0.87}
 model.param_bio_energy_conversion = {'hvo': 0.67, 'fame': .55}
+model.param_bio_dc = {'hvo': 0.89, 'fame': 0.32}
 model.param_bio_cost = {'hvo': 120, 'fame': 180}
 Min_ec = 0.11
-
 
 
 def _exp_bio_energy_content(model, fuel_type):
@@ -39,22 +40,61 @@ def _exp_bio_energy_content(model, fuel_type):
 model.exp_bio_energy_content = pyo.Expression(model.set_fuel_types, rule=_exp_bio_energy_content)
 
 
-def _exp_fuel_energy_content(model, fuel_type):
-    return sum((model.param_fuel_purchase[fuel] - sum(model.var_bio_purchase[fuel, center, supplier, fuel_type, bio]
+def _exp_bio_energy_content_dc(model, fuel_type):
+    return sum(model.var_bio_purchase[fuel, center, supplier, fuel_type, bio] *
+               model.param_bio_energy_conversion[bio] * model.param_bio_dc[bio]
+               for fuel in model.set_fuels
                for center in model.set_centers
                for supplier in model.set_suppliers
-               for bio in model.set_bios)) * model.param_fuel_energy_conversion[fuel]  for fuel in model.set_fuels)
+               for bio in model.set_bios)
+
+
+model.exp_bio_energy_content_dc = pyo.Expression(model.set_fuel_types, rule=_exp_bio_energy_content_dc)
+
+
+def _exp_fuel_energy_content(model, fuel_type):
+    return sum(
+        (model.param_fuel_purchase[fuel] -
+         sum(model.var_bio_purchase[fuel, center, supplier, fuel_type, bio]
+             for center in model.set_centers
+             for supplier in model.set_suppliers
+             for bio in model.set_bios)
+         ) * model.param_fuel_energy_conversion[fuel] for fuel in model.set_fuels)
 
 
 model.exp_fuel_energy_content = pyo.Expression(model.set_fuel_types, rule=_exp_fuel_energy_content)
 
 
 def _constraint_energy_content(model, fuel_type):
-    return 100* (model.exp_bio_energy_content[fuel_type] /
-    (model.exp_fuel_energy_content[fuel_type] + model.exp_bio_energy_content[fuel_type]) ) >= Min_ec
+    return (model.exp_bio_energy_content[fuel_type] + model.exp_bio_energy_content_dc[fuel_type]) \
+        >= Min_ec * (model.exp_fuel_energy_content[fuel_type] + model.exp_bio_energy_content[fuel_type])
 
 
 model.constraint_energy_content = pyo.Constraint(model.set_fuel_types, rule=_constraint_energy_content)
+
+def _constraint_max_volume_purchase(model, bio, fuel_type):
+    bio_quantity = sum(model.var_bio_purchase[fuel, center, supplier, fuel_type, bio]
+               for fuel in model.set_fuels
+               for center in model.set_centers
+               for supplier in model.set_suppliers)
+
+    return bio_quantity <= model.param_max_bio_volume[bio]
+
+
+model.constraint_max_volume_purchase = pyo.Constraint(model.set_bios, model.set_fuel_types, rule=_constraint_max_volume_purchase)
+
+
+def _constraint_min_volume_purchase(model, bio, fuel_type):
+    bio_quantity = sum(model.var_bio_purchase[fuel, center, supplier, fuel_type, bio]
+               for fuel in model.set_fuels
+               for center in model.set_centers
+               for supplier in model.set_suppliers)
+
+    return bio_quantity >= model.param_min_bio_volume[bio]
+
+
+model.constraint_min_volume_purchase = pyo.Constraint(model.set_bios, model.set_fuel_types,
+                                                      rule=_constraint_min_volume_purchase)
 
 
 def _objective_function(model):
@@ -67,6 +107,14 @@ def _objective_function(model):
 
 
 model.objective_function = pyo.Objective(rule=_objective_function, sense=pyo.minimize, doc='Minimize cost profit')
+
+
+def _exp_energy_content(model, fuel_type):
+    return (model.exp_bio_energy_content[fuel_type] + model.exp_bio_energy_content_dc[fuel_type]) / (
+                model.exp_fuel_energy_content[fuel_type] + model.exp_bio_energy_content[fuel_type])
+
+
+model.exp_energy_content = pyo.Expression(model.set_fuel_types, rule=_exp_energy_content)
 
 
 print('\n')
@@ -84,23 +132,39 @@ print('\n')
 print('\n')
 print('--- Model Results ---')
 
-
-
 # Check the solution
+
 if (results.solver.status == SolverStatus.ok) and \
         (results.solver.termination_condition == TerminationCondition.optimal):
-    print(':::Feasible solution:::')
+    print('Optimal Solution')
 
     print('--- Problem Results ---')
     print(results.Problem())
     print('--- Solver Results ---')
     print(results.Solver())
 
-    print('--- Objective (Profit) ---')
-    print('z:', pyo.value(model.objective_function))
-    print(model.objective_function.pprint())
+
+    for (key, var) in model.var_bio_purchase.items():
+        print('var_bio_purchase({}):{}'.format(key, pyo.value(var)))
+
+    for (key, var) in model.objective_function.items():
+        print('objective_function({}):{}'.format(key, pyo.value(var)))
+
+    for (key, var) in model.exp_bio_energy_content.items():
+        print('exp_bio_energy_content({}):{}'.format(key, pyo.value(var)))
+
+    for (key, var) in model.exp_bio_energy_content_dc.items():
+        print('exp_bio_energy_content_dc({}):{}'.format(key, pyo.value(var)))
+
+    for (key, var) in model.exp_fuel_energy_content.items():
+        print('exp_fuel_energy_content({}):{}'.format(key, pyo.value(var)))
+
+    for (fuel_type, var) in model.exp_energy_content.items():
+        print('exp_energy_content({}):{}'.format(fuel_type, pyo.value(var)))
+
 
     # Do something when the solution in optimal and feasible
+
 elif results.solver.termination_condition == TerminationCondition.infeasible:
     print('(!) Infeasible solution')
 else:
@@ -108,5 +172,3 @@ else:
     print('Status:', results.solver.status)
     # Something else is wrong
     print('termination condition:', results.solver.termination_condition)
-
-
